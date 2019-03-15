d0b57d990ddc62240b1567cbac1982dce11b5c52
Dave Hylands <dhylands@gmail.com>
2019-03-15 14:44:29 -0700

    Bundle libopenzwave with adapter

--- package.sh
+++ package.sh
@@ -26,14 +26,29 @@ else
   yarn install ${PRODUCTION}
 fi
 
+OZW_PKG="libopenzwave"
+OZW_DIR="openzwave"
+OZW_LIB_DIR="${OZW_DIR}/lib"
+OZW_CONFIG_DIR="${OZW_DIR}/config"
+OZW_LIB_VERSION="$(pkg-config --modversion ${OZW_PKG})"
+OZW_PKG_LIB_DIR="$(pkg-config --variable=libdir ${OZW_PKG})"
+OZW_LIB_NAME="${OZW_PKG_LIB_DIR}/libopenzwave.so.${OZW_LIB_VERSION}"
+
+rm -rf "${OZW_DIR}"
+mkdir -p "${OZW_LIB_DIR}" "${OZW_CONFIG_DIR}"
+cp -r "$(pkg-config --variable=sysconfdir ${OZW_PKG})/." "${OZW_CONFIG_DIR}/"
+cp "${OZW_LIB_NAME}" "${OZW_LIB_DIR}"
+
 rm -f SHA256SUMS
 sha256sum package.json *.js LICENSE > SHA256SUMS
-find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
+find "node_modules" -type f -exec sha256sum {} \; >> SHA256SUMS
+find "${OZW_DIR}" -type f -exec sha256sum {} \; >> SHA256SUMS
 TARFILE="$(npm pack)"
 tar xzf ${TARFILE}
 rm ${TARFILE}
 TARFILE_ARCH="${TARFILE/.tgz/${TARFILE_SUFFIX}.tgz}"
-cp -r node_modules ./package
-tar czf ${TARFILE_ARCH} package
-rm -rf package
+cp -r "node_modules" "./package"
+cp -r "${OZW_DIR}" "./package"
+tar czf ${TARFILE_ARCH} "package"
+rm -rf "package"
 echo "Created ${TARFILE_ARCH}"
